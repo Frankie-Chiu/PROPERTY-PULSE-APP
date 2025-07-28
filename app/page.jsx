320ee0c260a95f6511e3f95bb0a75821ee982c40
Frankie <frankiechao88@gmail.com>
2025-07-28 10:50:49 +1000

    Init commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "property, rental, real-estate",
+  description: "find the perfect rental property",
+};
+
+const HomePage = () => {
+  return <div className="text-2xl font-poppins">HomePage</div>;
+};
+
+export default HomePage;
